1e441dfa8b8dbd98f260d7cca1ffe3eaadca2cce
Frank Martinez <frankie.martinez@gmail.com>
2019-08-04 18:32:06 -0700

    Updated card search filters
    Started building search filters for colors, set, subtype. Started building pagination for results. Built a filter to not display cards with no picture, but to still account for them in results returned.

--- client/src/components/Cards/CardSearch.jsx
+++ client/src/components/Cards/CardSearch.jsx
@@ -8,18 +8,28 @@ import CardStatus from './CardStatus.jsx';
 class CardSearch extends Component {
     state = {
         cardSearchResults:[],
+        cardsWithPictures:[],
         searchResultPlaceholder:'Search for cards',
         inputCardName:'',
         status:null
     }
 
-    findCardByName = cardName => {
+    findCard = (searchName, searchSubtype, searchSet, searchColors, searchPage) => {
         mtg.card
         .where({
-            name: cardName, 
-            // language: 'english'
+            name: searchName,
+            subtypes: searchSubtype,
+            set: searchSet,
+            colors: searchColors,
+            page: searchPage,
         })
         .then(results => {
+            if(!searchName && !searchSubtype && !searchSet && !searchColors && !searchPage) {
+                this.setState({
+                    searchResultPlaceholder:'Please try again!',
+                })
+                return;
+            }
             if(results.length === 0) {
                 this.setState({
                     inputCardName:'',
@@ -34,14 +44,32 @@ class CardSearch extends Component {
             else {
                 this.setState({
                     cardSearchResults:results,
-                    searchResultPlaceholder:`Found ${results.length} cards`,
                     inputCardName:''
                 })
+                this.removeCardsWithNoPics();
             }
         })
+        .catch(error => {
+            this.setState({
+                searchResultPlaceholder:'Error fetching results!',
+                inputCardName:''
+            })
+            console.log(error)
+        })
     }
-    
 
+
+    removeCardsWithNoPics = () => {
+        const cardsWithPictures = this.state.cardSearchResults.filter(cardObject => cardObject.imageUrl)
+        let cardOrCards = 'card';
+        if (cardsWithPictures.length > 1) { cardOrCards='cards' };
+        this.setState({
+            cardsWithPictures:cardsWithPictures,
+            searchResultPlaceholder:`Found ${this.state.cardSearchResults.length} cards, displaying ${cardsWithPictures.length} ${cardOrCards}.`,
+        })
+    }
+
+    
     handleChanges = event => {
         this.setState({
             [event.target.name]:event.target.value
@@ -53,7 +81,7 @@ class CardSearch extends Component {
         this.setState({
             searchResultPlaceholder:"Searching..."
         })
-        this.findCardByName(this.state.inputCardName);
+        this.findCard(this.state.inputCardName);
     }
 
     render() {
@@ -70,6 +98,7 @@ class CardSearch extends Component {
                             onChange={this.handleChanges}
                             value={this.state.inputCardName}
                         />
+                        <button className="btn-dark" onClick={this.submitSearch}>Search</button>
                     </form>
                 </section>
 
@@ -81,7 +110,7 @@ class CardSearch extends Component {
                     
                     {this.props.status ? (<CardStatus status={this.props.status} />) : null}
 
-                    {this.state.cardSearchResults.map(card => {
+                    {this.state.cardsWithPictures.map(card => {
                         return (
                             <Fragment key={card.id}>
                                 <Card 
